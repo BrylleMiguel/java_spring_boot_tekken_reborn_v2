0b4d3e6314e639b6f6e58f7fb67cd8073c47e17e
brylle <miguelbrylle@gmail.com>
2023-02-09 21:06:18 -0500

    lab 3: done

--- src/main/java/com/cpan252/tekkenreborn/repository/FighterRepository.java
+++ src/main/java/com/cpan252/tekkenreborn/repository/FighterRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.cpan252.tekkenreborn.model.Fighter;
 
-public interface FighterRepository extends CrudRepository<Fighter, Long> {
+public interface FighterRepository extends CrudRepository<Fighter, String> {
+    // Iterable<Fighter> findAll();
+    // Optional<Fighter> findById(Long id);
+    // void save(Fighter fighter);
 }
